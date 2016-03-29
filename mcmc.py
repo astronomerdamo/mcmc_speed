ba161451baac85f64163a0ea04cf4de9c1a4f089
Damien Robertson <damien.robertson@shopify.com>
2016-03-28 23:27:58 -0400

    Fixed flake8 errors

--- mcmc.py
+++ mcmc.py
@@ -1,57 +1,56 @@
 '''
 
     Simple MCMC:
-    Utilizes a Metropolis-Hastings Algorithm with a 
+    Utilizes a Metropolis-Hastings Algorithm with a
     standard Gibbs sampler.
 
 '''
 
-from __future__ import print_function 
+from __future__ import print_function
 from numpy import random, exp
-import time 
+import time
 
 '''
     Basic linear equation function
 '''
 
-def lnfnc(x, a, b): 
-    return  [a*x + b for x in x]
+
+def lnfnc(x, a, b):
+    return [a*x + b for x in x]
 
 '''
     Basic chi squared function
     Note: data set error is variance not stddev
 '''
 
+
 def chisqr(yo, ye, syo):
     csq = [(yo - ye)**2 / syo for yo, ye, syo in zip(yo, ye, syo)]
     return sum(csq)
 
 '''
-    Basic Metropolis-Hastings Algorithm with a 
+    Basic Metropolis-Hastings Algorithm with a
     standard Gibbs sampler.
 '''
+
 
 def mcmc(a0, sa, b0, sb, x, y, dy):
 
     y0 = lnfnc(x, a0, b0)
     chi0 = chisqr(y0, y, dy)
-
     atrace = []
     btrace = []
-
     i = 0
     j = 0
     mcn = 500000
     brn = 1000
 
-    while i < mcn: 
+    while i < mcn:
         at = random.normal(a0, sa)
         bt = random.normal(b0, sb)
-
         yt = lnfnc(x, at, bt)
         chit = chisqr(yt, y, dy)
-        
-        acal = exp((chi0 - chit)/2.0)
+        acal = exp((chi0 - chit) / 2.0)
         aexp = min(1.0, acal)
         u = random.rand()
 
@@ -63,6 +62,7 @@ def mcmc(a0, sa, b0, sb, x, y, dy):
             atrace.append(at)
             btrace.append(bt)
             i += 1
+
         j += 1
 
     print("i: ", i, ", j: ", j)
@@ -96,4 +96,3 @@ sb = 2.5
 cpubeg = time.time()
 mcmc(a0, sa, b0, sb, x, y, dy)
 print("CPU time: ", time.time() - cpubeg)
-
